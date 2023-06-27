1ee9d9bff2e29a6fcb9289712e6aaf1f0b0f56b7
Betinsk <Gmdust@hotmail.com>
2023-06-27 13:37:07 -0300

    Criacao

---
+++ src/main/java/com/betinsk/academia/AcademiaJava2Application.java
@@ -0,0 +1,13 @@
+package com.betinsk.academia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaJava2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaJava2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/betinsk/academia/AcademiaJava2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betinsk.academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaJava2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
